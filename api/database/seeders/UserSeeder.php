7d5de8b815a6d3727ca2d1d3484e416f3cf85bcf
Muhammad Faizan <mfaizanrasool1@gmail.com>
2021-09-14 02:21:02 +0500

    more fixes

--- api/database/seeders/UserSeeder.php
+++ api/database/seeders/UserSeeder.php
@@ -18,37 +18,20 @@ class UserSeeder extends Seeder
             "id" => 1,
             "role_id" => 1,
             "is_super_admin" => 1,
-            "name" => 'Admin',
-            "email" => 'admin@dastak.com',
-            "phone" => '+923001234567',
+            "name" => 'Developer',
+            "email" => 'developer@capri.com',
+            "phone" => '+923016336171',
             "password" => 'admin',
         ]);
 
         User::create([
             "id" => 2,
-            "role_id" => 2,
-            "name" => 'Vendor',
-            "email" => 'vendor@dastak.com',
-            "phone" => '+923001234568',
-            "password" => 'vendor',
-        ]);
-
-        User::create([
-            "id" => 3,
-            "role_id" => 3,
-            "name" => 'Rider',
-            "email" => 'rider@dastak.com',
-            "phone" => '+923001234569',
-            "password" => 'rider',
-        ]);
-
-        User::create([
-            "id" => 4,
-            "role_id" => 4,
-            "name" => 'Customer',
-            "email" => 'customer@dastak.com',
-            "phone" => '+923001234560',
-            "password" => 'customer',
+            "role_id" => 1,
+            "is_super_admin" => 1,
+            "name" => 'client',
+            "email" => 'client@capri.com',
+            "phone" => '+923001234567',
+            "password" => 'client',
         ]);
     }
 }
