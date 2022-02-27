018fee90641ca766dd3b2af354e2a65f6729ed58
Muhammad Faizan <45095536+muhammadfaizan1@users.noreply.github.com>
2022-02-27 20:49:52 +0500

    capri logo

--- api/database/migrations/2022_01_09_180235_create_sales_table.php
+++ api/database/migrations/2022_01_09_180235_create_sales_table.php
@@ -17,11 +17,11 @@ class CreateSalesTable extends Migration
             $table->id();
             $table->boolean('is_return')->nullable()->default(false);
             $table->unsignedBigInteger('company_head_id');
-            $table->timestamp('date');
-            $table->integer('bill_no');
+            $table->timestamp('date')->index();
+            $table->integer('bill_no')->index();
             $table->integer('po_no');
             $table->integer('grn');
-            $table->string('party_name');
+            $table->string('party_name')->index();
             $table->enum('pay_mode', ['CREDIT', 'CASH'])->index();
             $table->enum('pay_status', ['PENDING', 'DONE'])->index();
             $table->string('remarks')->nullable();
