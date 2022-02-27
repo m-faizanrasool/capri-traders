0c193a882497da9d70407def4b2761bf8a96dafc
Muhammad Faizan <45095536+muhammadfaizan1@users.noreply.github.com>
2022-02-27 21:35:12 +0500

    purchases migrations

--- api/app/Models/CompanyHead.php
+++ api/app/Models/CompanyHead.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class CompanyHead extends Model
 {
     use HasFactory;
+
+    public function sales()
+    {
+        return $this->hasMany(Sale::class);
+    }
+    public function purchases()
+    {
+        return $this->hasMany(Purchase::class);
+    }
+
+    public function ledger()
+    {
+        $sales = $this->sales();
+        $purchases = $this->purcahses();
+        $ledger = $sales->merge($purchases);
+        dd($ledger);
+    }
 }
---
+++ api/database/migrations/2022_02_27_212828_create_purchases_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePurchasesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('purchases', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('is_return')->nullable()->default(false);
+            $table->unsignedBigInteger('company_head_id');
+            $table->timestamp('date')->index();
+            $table->integer('bill_no')->index();
+            $table->integer('po_no');
+            $table->integer('grn');
+            $table->string('party_name')->index();
+            $table->enum('pay_mode', ['CREDIT', 'CASH'])->index();
+            $table->enum('pay_status', ['PENDING', 'DONE'])->index();
+            $table->string('remarks')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('company_head_id')->references('id')->on('company_heads');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('purchases');
+    }
+}
