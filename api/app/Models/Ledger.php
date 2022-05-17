42b3f13ea7e086e17e55ce79387b6d9c6f63ac7d
Muhammad Faizan <45095536+muhammadfaizan1@users.noreply.github.com>
2022-05-17 17:39:04 +0500

    use softdeletes on ledger

--- api/app/Models/Ledger.php
+++ api/app/Models/Ledger.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Ledger extends Model
 {
-    protected $guarded = [];
+    use HasFactory, SoftDeletes;
 
-    use HasFactory;
+    protected $guarded = [];
 }
