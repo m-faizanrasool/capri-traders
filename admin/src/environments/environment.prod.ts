d6460cf016bd4ff51ecdd1fd7b6cfa5690f44aae
Muhammad Faizan <45095536+m-faizanrasool@users.noreply.github.com>
2022-07-17 18:00:47 +0500

    admin build

--- admin/src/environments/environment.prod.ts
+++ admin/src/environments/environment.prod.ts
@@ -2,7 +2,8 @@ export const environment = {
 	production: true,
 	appVersion: 'v717demo1',
 	USERDATA_KEY: 'authf649fc9a5f55',
-	apiUrl: 'http://ec2-34-240-203-182.eu-west-1.compute.amazonaws.com/api/',
+	apiUrl:
+		'http://ec2-34-240-203-182.eu-west-1.compute.amazonaws.com/api/public/api/',
 	// apiUrl: 'http://localhost/capri-traders/api/public/api/',
 	// apiUrl: 'https://capritraders.com/api/public/api/',
 	// apiUrl: 'https://capri-traders.herokuapp.com/api/',
